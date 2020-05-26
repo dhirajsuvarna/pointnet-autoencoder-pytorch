26f74809ce29625508519ee1f86aaae18e706ca6
unknown <dhiraj.suvarna@gmail.com>
2020-05-26 15:53:47 +0530

    dump output in green color, add an argument

--- infer.py
+++ infer.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--input_folder", required=True, help="Single 3d model or input folder containing 3d models")
 parser.add_argument("--nn_model", required=True, help="Trained Neural Network Model")
+parser.add_argument("--nn_model_type", required=True, choices=['fxia', 'dhiraj'], help="Model Type")
 parser.add_argument("--out_norm_input", action="store_true", help="Output normalized version of input file")
 
 ip_options = parser.parse_args()
@@ -17,16 +18,17 @@ input_folder = ip_options.input_folder
 
 # Setting the Gradient Calculation Feature off
 # torch.set_grad_enabled(False)
-
-
-device = torch.device('cpu')
-state_dict = torch.load(ip_options.nn_model, map_location=device)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 point_dim = 3
 num_points = 2048
 
-# autoencoder = PCAutoEncoder(point_dim, num_points)
-autoencoder = PointNetAE(num_points)
+if ip_options.nn_model_type == 'dhiraj':
+    autoencoder = PCAutoEncoder(point_dim, num_points)
+elif ip_options.nn_model_type == 'fxia':
+    autoencoder = PointNetAE(num_points)
+
+state_dict = torch.load(ip_options.nn_model, map_location=device)
 autoencoder.load_state_dict(state_dict)
 
 def save_as_pcd(iFileName, iPoints, iColor=None):
@@ -49,6 +51,7 @@ def generate_color(iPoints, iColor):
 
 def infer_model_file(input_file, autoencoder):
     
+    print(f"Processing file: {input_file}")
     cloud = o3d.io.read_point_cloud(input_file)
     points = np.array(cloud.points)
 
@@ -63,24 +66,24 @@ def infer_model_file(input_file, autoencoder):
 
     if ip_options.out_norm_input:
         norm_ip_file = get_path_without_ext(input_file) + "_norm.pcd"
-        green_color = (0, 255, 0)
-        color = generate_color(points, green_color)
-        save_as_pcd(norm_ip_file, points, color)
+        save_as_pcd(norm_ip_file, points)
 
     points = torch.from_numpy(points).float()
     points = torch.unsqueeze(points, 0) #done to introduce batch_size of 1 
     points = points.transpose(2, 1)
     #points = points.cuda() #uncomment this if running on GPU
     autoencoder = autoencoder.eval()
-    # reconstructed_points, global_feat = autoencoder(points)
-    reconstructed_points = autoencoder(points)
+    reconstructed_points, global_feat = autoencoder(points)
+    #reconstructed_points = autoencoder(points)
 
     #Reshape 
     reconstructed_points = reconstructed_points.squeeze().transpose(0,1)
     reconstructed_points = reconstructed_points.numpy()
     
     outPcdFile = get_path_without_ext(input_file) + "_out.pcd"
-    save_as_pcd(outPcdFile, reconstructed_points)
+    green_color = (0, 255, 0)
+    color = generate_color(reconstructed_points, green_color)
+    save_as_pcd(outPcdFile, reconstructed_points, color)
 
 
 
