2a7d5d563532e4f191405ab550f0447d4e52f84a
unknown <dhiraj.suvarna@gmail.com>
2020-05-26 09:51:10 +0530

    removing commented code

--- model/model_fxia22.py
+++ model/model_fxia22.py
@@ -29,24 +29,6 @@ class PointNetAE(nn.Module):
 
         return x
 
-# class PointNetCls(nn.Module):
-#     def __init__(self, num_points = 2500, k = 2):
-#         super(PointNetCls, self).__init__()
-#         self.num_points = num_points
-#         self.feat = PointNetfeat(num_points, global_feat=True)
-#         self.fc1 = nn.Linear(1024, 512)
-#         self.fc2 = nn.Linear(512, 256)
-#         self.fc3 = nn.Linear(256, k)
-#         self.bn1 = torch.nn.BatchNorm1d(512)
-#         self.bn2 = torch.nn.BatchNorm1d(256)
-#         self.relu = nn.ReLU()
-#     def forward(self, x):
-#         x, trans = self.feat(x)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return F.log_softmax(x), trans
-
 class STN3d(nn.Module):
     def __init__(self, num_points = 2500):
         super(STN3d, self).__init__()
